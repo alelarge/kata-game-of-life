63173872818013e66a7831e8dd17522e52c5c482
alelarge <armelle.lelarge@shodo.io>
2023-07-25 16:03:08 +0200

    begin kata game of life exept test acceptance

--- src/test/kotlin/TestAcceptanceTest.kt
+++ src/test/kotlin/TestAcceptanceTest.kt
@@ -1,22 +1,17 @@
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import org.assertj.core.api.Assertions.assertThat
 
 class TestAcceptanceTest {
     @Test
     fun any_living_cell_with_fewer_than_two_living_neighbors_dies_subpopulation() {
-        val gameOfLife = GameOfLife(5, 5)
-        gameOfLife.setCellState(1, 1, CellState.ALIVE)
-        gameOfLife.nextGeneration()
-        assertEquals(CellState.DEAD, gameOfLife.getCellState(1, 1))
-/*
-        val exception = assertThrows<IllegalArgumentException> { gameOfLife.nextGeneration()
-*/
+        
     }
 
     @Test
     fun any_living_cell_with_two_or_three_living_neighbors_survives() {
-
     }
+
 
     @Test
     fun any_living_cell_with_more_than_three_living_neighbors_dies_overpopulation() {
