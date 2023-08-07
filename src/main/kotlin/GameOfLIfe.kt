c1ad43821bd4c9667f44b21c2d385cfc22ff89d7
alelarge <armelle.lelarge@shodo.io>
2023-08-07 15:20:57 +0200

    refactor

--- src/main/kotlin/GameOfLIfe.kt
+++ src/main/kotlin/GameOfLIfe.kt
@@ -1,36 +1,40 @@
 interface CellState {
     fun isAlive(): Boolean
+    fun evolve(neighbours: List<Cell>): CellState
 }
 
 class AliveState : CellState {
     override fun isAlive() = true
+
+    override fun evolve(neighbours: List<Cell>): CellState {
+        val numNeighboursAlive = neighbours.count { it.state.isAlive() }
+        return if (numNeighboursAlive < 2 || numNeighboursAlive > 3) {
+            DeadState()
+        } else {
+            AliveState()
+        }
+    }
 }
 
 class DeadState : CellState {
     override fun isAlive() = false
+
+    override fun evolve(neighbours: List<Cell>): CellState {
+        val numNeighboursAlive = neighbours.count { it.state.isAlive() }
+        return if (numNeighboursAlive == 3) {
+            AliveState()
+        } else {
+            DeadState()
+        }
+    }
 }
 
-class Cell(private var state: CellState) {
+class Cell(var state: CellState) {
     fun isAlive(): Boolean {
         return state.isAlive()
     }
 
     fun evolve(neighbours: List<Cell>) {
-        val numNeighboursAlive = neighbours.count { it.isAlive() }
-
-        state = if (state.isAlive()) {
-            if (numNeighboursAlive < 2 || numNeighboursAlive > 3) {
-                DeadState()
-            } else {
-                AliveState()
-            }
-        } else {
-            if (numNeighboursAlive == 3) {
-                AliveState()
-            } else {
-                DeadState()
-            }
-        }
+        state = state.evolve(neighbours)
     }
 }
-
