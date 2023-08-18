5b6bc7904c5c72432d26a0d24a046925cd133509
alelarge <armelle.lelarge@shodo.io>
2023-08-18 23:36:48 +0200

    refactor test cell with 3 live neighbour survives

--- src/test/kotlin/GameOfLifeTest.kt
+++ src/test/kotlin/GameOfLifeTest.kt
@@ -99,11 +99,14 @@ class CellTest {
     fun `cell with 3 live neighbors survives to the next generation`() {
         val aliveCell = Cell(CellState.ALIVE)
         val neighbors = List(3) { Cell(CellState.ALIVE) }
-        neighbors.add(Cell(CellState.DEAD))
-        aliveCell.evolve(neighbors)
+        val deadNeighbor = Cell(CellState.DEAD)
+        neighbors.forEach { aliveCell.addNeighbor(it) }
+        aliveCell.addNeighbor(deadNeighbor)
+        aliveCell.evolve()
+
         assertThat(aliveCell.isAlive()).isTrue
-        }
     }
+}
 
 
 
